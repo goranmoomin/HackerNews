088928059ee597b74f855e6335cc7eb48c4f30a0
Sungbin Jo <pcr910303@icloud.com>
2020-03-20 18:22:07 +0900

    Remove unneeded code

--- HackerNews/Views/DisclosureTrianglelessOutlineView.swift
+++ HackerNews/Views/DisclosureTrianglelessOutlineView.swift
@@ -8,13 +8,4 @@ class DisclosureTrianglelessOutlineView: NSOutlineView {
     override func frameOfOutlineCell(atRow row: Int) -> NSRect {
         .zero
     }
-
-    override func frameOfCell(atColumn column: Int, row: Int) -> NSRect {
-        var frame = super.frameOfCell(atColumn: column, row: row)
-
-        frame.origin.x -= indentationPerLevel
-        frame.size.width += indentationPerLevel
-
-        return frame
-    }
 }
