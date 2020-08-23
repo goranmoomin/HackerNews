6fb1c01657755df6cd4292b1c50a15943fcb17ab
Sungbin Jo <pcr910303@icloud.com>
2020-08-24 01:03:10 +0900

    Add text story support

--- HackerNews/Extensions/Atributika.swift
+++ HackerNews/Extensions/Atributika.swift
@@ -1,5 +1,5 @@
 
-import Foundation
+import Cocoa
 import Atributika
 
 extension TagTransformer {
@@ -14,5 +14,33 @@ extension TagTransformer {
             }
         }
         return transformer
+    }
+}
+
+extension String {
+    var styledAttributedString: NSAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.paragraphSpacing = 8
+        let systemFontSize = NSFont.systemFontSize(for: .regular)
+        let all = Style
+            .font(.systemFont(ofSize: systemFontSize))
+            .paragraphStyle(paragraphStyle)
+        let a = Style("a")
+        let i = Style("i")
+            .font(.italicSystemFont(ofSize: systemFontSize))
+        let pre = Style("pre")
+            .font(.monospacedSystemFont(ofSize: systemFontSize, weight: .regular))
+        let transformers: [TagTransformer] = [
+            .pTransformer(),
+            .brTransformer
+        ]
+        func tuner(style: Style, tag: Tag) -> Style {
+            if tag.name == a.name, let href = tag.attributes["href"], let url = URL(string: href) {
+                return style.link(url)
+            }
+            return style
+        }
+        return self.style(tags: a, i, pre, transformers: transformers, tuner: tuner(style:tag:))
+            .styleAll(all).attributedString
     }
 }
