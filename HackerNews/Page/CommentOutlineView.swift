59abeb65cca3887b8f7a1293c81e5143b061f008
Sungbin Jo <goranmoomin@daum.net>
2021-03-10 00:08:19 +0900

    Fix outline cell frame to be more stable

--- HackerNews/Page/CommentOutlineView.swift
+++ HackerNews/Page/CommentOutlineView.swift
@@ -4,7 +4,8 @@ import Cocoa
 class CommentOutlineView: NSOutlineView {
 
     override func frameOfOutlineCell(atRow row: Int) -> NSRect {
+        let rowRect = rect(ofRow: row)
         let frame = super.frameOfOutlineCell(atRow: row)
-        return NSRect(x: frame.minX, y: frame.minY + 1, width: frame.width, height: 18)
+        return NSRect(x: frame.minX, y: rowRect.minY + 2, width: frame.width, height: 18)
     }
 }
