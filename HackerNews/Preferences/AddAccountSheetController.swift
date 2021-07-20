0de6d0869b654e18bcc2301eee54a76fda5bdaad
Sungbin Jo <goranmoomin@daum.net>
2021-07-21 08:58:28 +0900

    Add login progress indicator

--- HackerNews/Preferences/AddAccountSheetController.swift
+++ HackerNews/Preferences/AddAccountSheetController.swift
@@ -5,6 +5,7 @@ class AddAccountSheetController: NSViewController {
 
     @IBOutlet var usernameTextField: NSTextField!
     @IBOutlet var passwordTextField: NSTextField!
+    @IBOutlet var spinner: NSProgressIndicator!
 
     override func viewDidLoad() {
         super.viewDidLoad()  // Do view setup here.
@@ -13,16 +14,19 @@ class AddAccountSheetController: NSViewController {
     @IBAction func signIn(_ sender: NSButton) {
         let username = usernameTextField.stringValue
         let password = passwordTextField.stringValue
+        usernameTextField.isEditable = false
+        passwordTextField.isEditable = false
+        spinner.startAnimation(self)
         APIClient.shared.login(userName: username, password: password) { result in
-            switch result {
-            case .success(let token):
-                Account.addAccount(Account(username: username, password: password))
-                Account.selectAccount(withUsername: username)
-                Token.current = token
-            case .failure(let error):
-                DispatchQueue.main.async { NSApplication.shared.presentError(error) }
-            }
             DispatchQueue.main.async {
+                self.spinner.stopAnimation(self)
+                switch result {
+                case .success(let token):
+                    Account.addAccount(Account(username: username, password: password))
+                    Account.selectAccount(withUsername: username)
+                    Token.current = token
+                case .failure(let error): NSApplication.shared.presentError(error)
+                }
                 let preferencesViewController =
                     self.presentingViewController as! PreferencesViewController
                 self.dismiss(self)
